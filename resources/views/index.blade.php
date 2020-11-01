3959eee955c77b448dd1d4031276ad9653c79805
1 <abdounovich@gmail.com>
2020-11-01 20:47:34 +0100

    'comment'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,68 +16,81 @@
 @endif
 
  
-<div class="container">
-    <div class="card-group text-center">
-
-        <div class="card bg-info d-flex align-self-center m-2 p-2"style="opacity: 0.9">
-            <div class="card-body text-center" >
-                <i class="  p-2 fa fa-list-ol fa-5x "></i>
-            </div>
-            <div class="h3 p-2 text-light d-flex align-self-center"> Produits   </div>
-            <div class="h3 p-2 text-light d-flex align-self-center">   {{$products->count()}} </div>
-            <a  href="{{route('products')}}" class=" p-2 mb-4  btn btn-primary d-flex align-self-center"> afficher tout </a>
-        </div> 
-
-
-        <div class="card bg-info d-flex align-self-center m-2 p-2 "style="opacity: 0.9">
-            <div class="card-body text-center" >
-                <i class="  p-2 fa fa-list-ol fa-5x "></i>
-            </div>
-            <div class="h3 p-2 text-light d-flex align-self-center"> Commandes   </div>
-            <div class="h3 p-2 text-light d-flex align-self-center">   {{$commandes->count()}} </div>
-            <a  href="{{route('commandes')}}" class=" p-2 mb-4  btn btn-primary d-flex align-self-center"> afficher tout </a>
-        </div> 
-
-
-
-        <div class="card bg-info d-flex align-self-center m-2 p-2 "style="opacity: 0.9">
-            <div class="card-body text-center" >
-                <i class="  p-2 fa fa-list-ol fa-5x "></i>
-            </div>
-            <div class="h3 p-2 text-light d-flex align-self-center"> Clients   </div>
-            <div class="h3 p-2 text-light d-flex align-self-center">   {{$clients->count()}} </div>
-            <a  href="{{route('clients')}}" class=" p-2 mb-4  btn btn-primary d-flex align-self-center"> afficher tout </a>
-        </div> 
-
-
-
-        <div class="card bg-info d-flex align-self-center m-2 p-2 "style="opacity: 0.9">
-            <div class="card-body text-center" >
-                <i class="  p-2 fa fa-list-ol fa-5x "></i>
-            </div>
-            <div class="h3 p-2 text-light d-flex align-self-center"> Categories   </div>
-            <div class="h3 p-2 text-light d-flex align-self-center">   {{$categories->count()}} </div>
-            <a  href="{{route('categories')}}" class=" p-2 mb-4  btn btn-primary d-flex align-self-center"> afficher tout </a>
-        </div> 
-
-
-
-
-        <div class="card bg-info d-flex align-self-center m-2 p-2 "style="opacity: 0.9">
-            <div class="card-body text-center" >
-                <i class="  p-2 fa fa-list-ol fa-5x "></i>
-            </div>
-            <div class="h3 p-2 text-light d-flex align-self-center"> Sous-categories    </div>
-            <div class="h3 p-2 text-light d-flex align-self-center">   {{$sub_categories->count()}} </div>
-            <a  href="{{route('sub_categories')}}" class=" p-2 mb-4  btn btn-primary d-flex align-self-center"> afficher tout </a>
-        </div> 
-
+<div class="container mt-5">
+  <div class="card-group text-white" style="opacity: 0.8">
   
+    <div class="card bg-info ">
+      <div class="card-body text-left ">
+      <a class="h4" href="{{route('products')}}"> 
+      <p class="card-text">
+      <i class=" p-2 mr-2 fa fa-list-ol ">
+      </i>Produits : {{$products->count()}}
+      </p>
+      </a>
+
+      </div>
     </div>
-  </div>
   
+    <div class="card bg-info ">
+        <div class="card-body text-left ">
+        <a class="h4" href="{{route('clients')}}"> 
+        <p class="card-text">
+        <i class=" p-2 mr-2 fa fa-users ">
+        </i>Clients : {{$clients->count()}}
+        </p>
+        </a>
+  
+        </div>
+      </div>
 
 
+
+
+      <div class="card bg-info ">
+        <div class="card-body text-left ">
+        <a class="h4" href="{{route('commandes')}}"> 
+        <p class="card-text">
+        <i class=" p-2 mr-2 fa fa-list-ol ">
+        </i>Commandes : {{$commandes->count()}}
+        </p>
+        </a>
+  
+        </div>
+      </div>
+
+
+
+
+      <div class="card bg-info ">
+        <div class="card-body text-left ">
+        <a class="h4" href="{{route('categories')}}"> 
+        <p class="card-text">
+        <i class=" p-2 mr-2 fa fa-list-ol ">
+        </i>Categories : {{$categories->count()}}
+        </p>
+        </a>
+  
+        </div>
+      </div>
+
+      <div class="card bg-info ">
+        <div class="card-body text-left ">
+        <a class="h4" href="{{route('sub_categories')}}"> 
+        <p class="card-text">
+        <i class=" p-2 mr-2 fa fa-list-ol ">
+        </i>Sous-categories : {{$sub_categories->count()}}
+        </p>
+        </a>
+  
+        </div>
+      </div>
+
+  </div>
+</div>
+
+
+
+  
 
 
 
