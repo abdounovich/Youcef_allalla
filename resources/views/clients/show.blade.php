6a76e79b607f58482431bdc4526130713947eb43
1 <abdounovich@gmail.com>
2020-11-02 22:27:36 +0100

    'comment'

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -19,18 +19,55 @@ img {
             <h1 class="display-4 shadow bg-dark" style="opacity: 0.9">Liste des clients</h1>
         </div>
     </div>
+
+
+<div class="container">
+    <div class="row">
+        @foreach ($clients as $client)
+            <div class=" clearfix col col-12 bg-dark my-2 rounded text-white" style="opacity: 0.9">
+            <div class=" float-left my-2 mr-2"><img style="width: 100px; height:100px" src="{{$client->photo}}"  class=" img-thumbnail" alt=""></div>
+            <div class="mt-2 ">{{$client->facebook}}  <div class="float-right btn btn-danger rounded-circle "> 
+                <a href="{{route('clients.delete',$client->id) }}">
+                    <i class="  text-white fa fa-trash"></i>
+                </a>
+            </div></div>
+            <div class="mt-1 ">Telepone : {{$client->phone}}</div>
+            <div class="mt-1 ">Commandes : {{$client->commandes->count()}}</div>
+            <div class="mt-1 ">Address : {{$client->address}}</div>
+
+
+
+            </div>
+        @endforeach
+    </div>
+</div>
+
+
+
+
     <div class="row" >
         <div class="col-lg-8 mx-auto">
             <!-- List group-->
             <ul class="list-group  shadow-lg">
                 <!-- list group item-->
                 @foreach ($clients as $client)
+
+
+
+
+
+
+
                 <li class="list-group-item  shadow">
                     <!-- Custom content-->
 
                         
                     <div class="media align-items-lg-center flex-column flex-lg-row ">
                         <div class="media-body order-2 order-lg-1">
+                        </div class="clearfix">
+                        <div class="float-right">
+                        <img src="{{$client->photo}}" alt="Generic placeholder image" style="width:100px;height:100px" class=" mb-5 img-thumbnail  with-3d-shadow">
+                    </div> </div>
                             <div class=" h5 mt-0 font-weight-bold mb-2"><a href="{{route('client.show',$client->slug) }}">
                                 {{$client->facebook}}
                             </a> 
@@ -53,15 +90,14 @@ img {
                                 <h6 class="font-weight-bold my-2">@if ($client->commandes->count()=="0")
                                     il y'a aucune commande
                                 @else
-                                <a href="">nombre de commandes : {{$client->commandes->count()}}</a> @endif</h6>
+                                <a href="">commandes : {{$client->commandes->count()}}</a> @endif</h6>
                                                           
 
                             </div>
                           
                          
                          
-                        </div><img src="{{$client->photo}}" alt="Generic placeholder image" width="200" class=" rounded border border-dark with-3d-shadow ml-lg-5  order-1 order-lg-2">
-                    </div> <!-- End -->
+                       <!-- End -->
 
                 </li>                    @endforeach
  <!-- End -->
