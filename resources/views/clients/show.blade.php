adba843c73152d7250a241822c38018ecbfa5703
1 <abdounovich@gmail.com>
2020-11-02 23:24:02 +0100

    'comment'

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -37,7 +37,7 @@ img {
             </div>
             <div class=" text-white mr-2">
                 <div class=" float-right col col-12  "><i class=" text-warning fa fa-shopping-cart  "></i><span class="mr-5 ml-2 ">commandes : {{$client->commandes->count()}}</span>
-                    <i class=" text-warning fa fa-phone  "></i><span class="mx-2">  {{$client->phone}}</span></div>
+                    <i class=" text-info fa fa-phone  "></i><span class="mx-2">  {{$client->phone}}</span></div>
            
         </div>
 
