9fb9d5bca80e94275da3b8dd7777dee44a634f24
1 <abdounovich@gmail.com>
2020-11-02 20:19:01 +0100

    'comment'

--- resources/views/products/add.blade.php
+++ resources/views/products/add.blade.php
@@ -1,8 +1,25 @@
-
-   <div class=" fixed-bottom d-flex justify-content-end ">
+<style>
+  .btn-circle {
+    width: 30px;
+    height: 30px;
+    text-align: center;
+    padding: 6px 0;
+    font-size: 12px;
+    line-height: 1.428571429;
+    border-radius: 15px;
+  }
+  .btn-circle.btn-lg {
+    width: 50px;
+    height: 50px;
+    padding: 10px 16px;
+    font-size: 18px;
+    line-height: 1.33;
+    border-radius: 25px;
+  }</style>
+   <div class=" fixed-top mt-4 d-flex justify-content-end ">
     <a class="btn btn-primary   rounded  rounded-circle my-5 mr-5"  
-    type="button" data-toggle="collapse" data-target="#product_collapse" aria-expanded="false" aria-controls="collapseExample">
-      <i class="fa fa-plus fa-2x text-white" ></i></a>
+    type="button" data-toggle="collapse" data-target="#product_collapse" aria-expanded="false" aria-controls="collapseExample"
+    class="btn btn-info btn-circle text-white btn-lg"><i class="fa fa-plus"></i></a>
 
  </div>
 
