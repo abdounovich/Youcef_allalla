88eef15892d37747cd8288fc92d43148864a7867
1 <abdounovich@gmail.com>
2020-11-06 11:01:05 +0100

    'comment'

--- resources/views/commandes/non_confirmed.blade.php
+++ resources/views/commandes/non_confirmed.blade.php
@@ -1,6 +1,6 @@
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
-        <h1 class=" h4 mt-4 p-3 shadow bg-dark rounded" style="opacity: 0.9"> commandes non confirmées</h1>
+    <h1 class=" h4 mt-4 p-3 shadow bg-dark rounded" style="opacity: 0.9"> commandes non confirmées :{{$inactive_commandes->count}}</h1>
     </div>
 </div>
     @foreach ($inactive_commandes as $commande)
