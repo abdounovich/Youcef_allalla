529f581d0138f4cdb9542d1620971653ce10507e
1 <abdounovich@gmail.com>
2021-01-08 21:12:46 +0100

    'comment'

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,10 +20,10 @@ background: linear-gradient(90deg, #2C73D2 0%, #0089BA 100%);}</style>
 <div class="container  mt-5">
   <div class=" text-white" style="opacity: 0.9">
   
-    <div class="card bg-c-blue   m-2 border-dark">
+    <div class="card bg-light  m-2 ">
       <div class="card-body text-left ">
       <a class="h4" href="{{route('products')}}"> 
-      <p class="card-text text-light">
+      <p class="card-text text-primary">
       <i class=" p-2 mr-2 fa fa-list-ol ">
       </i>Produits : {{$products->count()}}
       </p>
@@ -32,10 +32,10 @@ background: linear-gradient(90deg, #2C73D2 0%, #0089BA 100%);}</style>
       </div>
     </div>
   
-    <div class="card bg-c-blue m-2 border-dark">
+    <div class="card bg-light m-2 ">
         <div class="card-body text-left ">
         <a class="h4" href="{{route('clients')}}"> 
-        <p class="card-text text-light">
+        <p class="card-text text-primary">
         <i class=" p-2 mr-2 fa fa-users ">
         </i>Clients : {{$clients->count()}}
         </p>
