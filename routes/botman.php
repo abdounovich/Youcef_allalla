e575164cfe02635329680eb9bdfa5818a2fac820
1 <abdounovich@gmail.com>
2021-01-16 15:42:50 +0100

    'comment'

--- routes/botman.php
+++ routes/botman.php
@@ -26,7 +26,7 @@ use BotMan\Drivers\Facebook\Extensions\GenericTemplate;
 
 $botman = resolve('botman');
 
-$botman->hears('GET_STARTED', function ($bot) {
+$botman->hears('بدأ الدردشة ', function ($bot) {
     $user = $bot->getUser();
     $facebook_id = $user->getId();
     // Access last name
