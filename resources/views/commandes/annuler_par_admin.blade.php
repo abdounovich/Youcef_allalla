d77c2373d1ee05d5aacec4ef14345e83901525ac
1 <abdounovich@gmail.com>
2021-01-16 19:46:14 +0100

    'comment'

--- resources/views/commandes/annuler_par_admin.blade.php
+++ resources/views/commandes/annuler_par_admin.blade.php
@@ -1,6 +1,6 @@
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
-    <h1 class=" h4 mt-4 p-3 shadow bg-dark rounded" style="opacity: 0.9"> commandes non confirmées :  {{$annuler_par_admin_count}}</h1>
+    <h1 class=" h4 mt-4 p-3 shadow bg-dark rounded" style="opacity: 0.9"> commandes annuler par admin :  {{$annuler_par_admin_count}}</h1>
     </div>
 </div>
     @foreach ($annuler_par_admin as $commande)
