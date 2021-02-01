ee86128c142384ef9f2be5ae2fe46e5a965f927b
1 <abdounovich@gmail.com>
2021-02-01 20:30:22 +0100

    'comment'

--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -1,32 +1,32 @@
 
 
 
-<a class="btn btn-outline-light  btn-circle float-right mt-2 mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
+<a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
     <span class=" text-white  fa fa-list  "></span>
 </a>
 
-<a class="btn btn-outline-light  btn-circle float-right mt-2 mr-1" href="{{route('commandes.delete',$commande->id)}}"  >
+<a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="{{route('commandes.delete',$commande->id)}}"  >
     <span class=" text-white  fa fa-trash  "></span>
 </a>
 
 @if ($commande->type=="1" OR $commande->type=="2" OR $commande->type=="3" OR $commande->type=="6" )
-<a class="btn btn-outline-light btn-circle float-right mt-2 mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
+<a class="btn btn-light btn-circle float-right mt-2 mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
     <span class=" text-white  fa fa-remove  "></span>
 </a>
 @endif
 
 @if ($commande->type=="2")
-<a class="btn btn-outline-success btn-circle float-right  mt-2 mr-1" href="{{route('commandes.delivration',$commande->id) }}">
+<a class="btn btn-success btn-circle float-right  mt-2 mr-1" href="{{route('commandes.delivration',$commande->id) }}">
     <span class="text-white  fa fa-truck   "></span>
 </a> 
 
-<a class="btn btn-outline-warning btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
+<a class="btn btn-warning btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
     <span class="text-white  fa fa-refresh    "></span>
 </a>
 @endif
 
 @if ($commande->type=="1")
-<a class="btn btn-outline-info btn-circle float-right  mt-2 mr-1" href="{{route('commandes.confirmation',$commande->id) }}">
+<a class="btn btn-info btn-circle float-right  mt-2 mr-1" href="{{route('commandes.confirmation',$commande->id) }}">
     <span class="text-white  fa fa-arrow-down    "></span>
 </a> 
 @endif
