d96f2d80e47a46f08e297958fd7a057a40c6c2c2
1 <abdounovich@gmail.com>
2021-02-01 20:20:50 +0100

    'comment'

--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -2,10 +2,10 @@
 
 
 <a class="btn btn-outline-light  btn-circle float-right mt-2 mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
-    <span class=" text-white  fa fa-list border-dark "></span>
+    <span class=" text-white  fa fa-list  "></span>
 </a>
 <a class="btn btn-outline-danger btn-circle float-right mt-2 mr-1" href="{{route('commandes.delete',$commande->id)}}">
-    <span class=" text-white  fa fa-trash border-dark "></span>
+    <span class=" text-white  fa fa-trash  "></span>
 </a>
 @if ($commande->type=="1" OR $commande->type=="2" OR $commande->type=="3" OR $commande->type=="6" )
 <a class="btn btn-outline-light btn-circle float-right mt-2 mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
