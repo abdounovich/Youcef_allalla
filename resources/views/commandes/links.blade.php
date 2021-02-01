6e72ab39723ef165bdf06b5c05b1c49e6e50c1fa
1 <abdounovich@gmail.com>
2021-02-01 21:12:41 +0100

    'comment'

--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -1,40 +1,51 @@
 
-<a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
-    <span class="     fa fa-list  "></span>
-</a>
 
-<a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="{{route('commandes.delete',$commande->id)}}"  >
-    <span class="     fa fa-trash  "></span>
-</a>
+<div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+     <i class="fa fa-ellipsis-v text-white"></i>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
+            <span class="     fa fa-list  "></span>
+        </a>
+        
+        <a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="{{route('commandes.delete',$commande->id)}}"  >
+            <span class="     fa fa-trash  "></span>
+        </a>
+        
+        @if ($commande->type=="1" OR $commande->type=="2" OR $commande->type=="3" OR $commande->type=="6" )
+        <a class="btn btn-light btn-circle float-right mt-2 mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
+            <span class="     fa fa-remove  "></span>
+        </a>
+        @endif
+        
+        @if ($commande->type=="2")
+        <a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.delivration',$commande->id) }}">
+            <span class="  fa fa-truck   "></span>
+        </a> 
+        
+        <a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
+            <span class="    fa fa-refresh    "></span>
+        </a>
+        @endif
+        
+        @if ($commande->type=="1")
+        <a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.confirmation',$commande->id) }}">
+            <span class="    fa fa-arrow-down    "></span>
+        </a> 
+        @endif
+        
+        
+        @if ($commande->type=="6")
+        <a class="btn btn-outline-success btn-circle float-right  mt-2 mr-1" href="{{route('commandes.done',$commande->id) }}">
+            <span class="    fa fa-check   "></span>
+        </a> 
+        <a class="btn btn-outline-warning btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
+            <span class="    fa fa-refresh  "></span>
+        </a> 
+        @endif
+        
+    </div>
+  </div>
 
-@if ($commande->type=="1" OR $commande->type=="2" OR $commande->type=="3" OR $commande->type=="6" )
-<a class="btn btn-light btn-circle float-right mt-2 mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
-    <span class="     fa fa-remove  "></span>
-</a>
-@endif
 
-@if ($commande->type=="2")
-<a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.delivration',$commande->id) }}">
-    <span class="  fa fa-truck   "></span>
-</a> 
-
-<a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
-    <span class="    fa fa-refresh    "></span>
-</a>
-@endif
-
-@if ($commande->type=="1")
-<a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.confirmation',$commande->id) }}">
-    <span class="    fa fa-arrow-down    "></span>
-</a> 
-@endif
-
-
-@if ($commande->type=="6")
-<a class="btn btn-outline-success btn-circle float-right  mt-2 mr-1" href="{{route('commandes.done',$commande->id) }}">
-    <span class="    fa fa-check   "></span>
-</a> 
-<a class="btn btn-outline-warning btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
-    <span class="    fa fa-refresh  "></span>
-</a> 
-@endif
