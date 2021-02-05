a08c5296db2c26f69d0401ff2e77cc1c37538dfd
1 <abdounovich@gmail.com>
2021-02-05 22:19:01 +0100

    'comment'

--- resources/views/commandes/color.blade.php
+++ resources/views/commandes/color.blade.php
@@ -39,7 +39,7 @@
                             if ($commande->created_at>$remises->created_at) {
                                 $percentage=round(100-$remises->prix*100/$remises->produit->prix);
 
-                        echo '   <p class="card-text mt-3 h5  "> <del class="text-dark">'.$commande->product->prix.' da  </del> 
+                        echo '   <p class="card-text mt-3 h5  "> <del class="text-danger">'.$commande->product->prix.' da  </del> 
                             <span class="text-white ml-3">'.$remises->prix.' da</span>                      
                             <span class="badge p-1 ml-3 badge-info"> - '.$percentage.' % </span>
                         </p>';
--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -1,51 +1,54 @@
 
 
-<div class="dropdown">
+{{-- <div class="dropdown">
     <a class="  float-right " type="button" id="dropdownMenuButton" data-toggle="dropdown"  aria-expanded="false">
      <i style="font-size: 20px" class="fa fa-ellipsis-v text-white"></i>
     </a>
-    <div class="dropdown-menu bg-dark" aria-labelledby="dropdownMenuButton">
-        <a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
+</div> --}}
+
+
+    <div class=" bg-dark" aria-labelledby="">
+        <a class="btn btn-danger  btn-circle float-right  mr-1" href="{{route('commandes.delete',$commande->id)}}"  >
+            <span class="     fa fa-trash  "></span>
+        </a>
+        <a class="btn btn-info  btn-circle float-right  mr-1" href="#"  data-toggle="collapse" data-target="#product_collapse{{$commande->id}}" aria-expanded="false" aria-controls="collapseExample">
             <span class="     fa fa-info  "></span>
         </a>
         
-        <a class="btn btn-light  btn-circle float-right mt-2 mr-1" href="{{route('commandes.delete',$commande->id)}}"  >
-            <span class="     fa fa-trash  "></span>
-        </a>
+      
         
-        @if ($commande->type=="1" OR $commande->type=="2" OR $commande->type=="3" OR $commande->type=="6" )
-        <a class="btn btn-light btn-circle float-right mt-2 mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
+        @if ($commande->type=="1" OR $commande->type=="2"  OR $commande->type=="6" )
+        <a class="btn btn-outline-danger  btn-circle float-right  mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
             <span class="     fa fa-remove  "></span>
         </a>
         @endif
         
         @if ($commande->type=="2")
-        <a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.delivration',$commande->id) }}">
+        <a class="btn btn-success btn-circle float-right   mr-1" href="{{route('commandes.delivration',$commande->id) }}">
             <span class="  fa fa-truck   "></span>
         </a> 
         
-        <a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
+        <a class="btn btn-warning btn-circle float-right   mr-1" href="{{route('commandes.return',$commande->id) }}">
             <span class="    fa fa-refresh    "></span>
         </a>
         @endif
         
         @if ($commande->type=="1")
-        <a class="btn btn-light btn-circle float-right  mt-2 mr-1" href="{{route('commandes.confirmation',$commande->id) }}">
+        <a class="btn btn-info btn-circle float-right   mr-1" href="{{route('commandes.confirmation',$commande->id) }}">
             <span class="    fa fa-arrow-down    "></span>
         </a> 
         @endif
         
         
         @if ($commande->type=="6")
-        <a class="btn btn-outline-success btn-circle float-right  mt-2 mr-1" href="{{route('commandes.done',$commande->id) }}">
+        <a class="btn btn-success btn-circle float-right mr-1" href="{{route('commandes.done',$commande->id) }}">
             <span class="    fa fa-check   "></span>
         </a> 
-        <a class="btn btn-outline-warning btn-circle float-right  mt-2 mr-1" href="{{route('commandes.return',$commande->id) }}">
+        <a class="btn btn-warning btn-circle float-right   mr-1" href="{{route('commandes.return',$commande->id) }}">
             <span class="    fa fa-refresh  "></span>
         </a> 
         @endif
         
     </div>
-  </div>
 
 
