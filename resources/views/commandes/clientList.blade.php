ba3dc95ded6f680f3bde35d2a5d34eb620289212
1 <abdounovich@gmail.com>
2021-02-05 23:33:40 +0100

    'comment'

--- resources/views/commandes/clientList.blade.php
+++ resources/views/commandes/clientList.blade.php
@@ -1,6 +1,6 @@
 <div class="  d-flex justify-content-center " >
 
-    <div class=" collapse col col-12 bg-light mb-2 rounded  text-white"  id="product_collapse{{$commande->id}}" style="opacity: 0.9">
+    <div class=" collapse col col-12 bg-dark mb-2 rounded  text-white"  id="product_collapse{{$commande->id}}" style="opacity: 0.9">
         <div class=" clearfix col col-12 bg-light  mb-2 p-2 rounded " style="opacity: 0.9">
             <div class="bg-light">
                 <div class=" float-left my-2 mr-2">
