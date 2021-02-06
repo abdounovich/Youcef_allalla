cb097a72ebd4d8697ea3002d4e7752dc79e0c5df
1 <abdounovich@gmail.com>
2021-02-06 15:17:31 +0100

    'comment'

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -17,7 +17,7 @@
                                  <div class="card-text h5 clearfix  ">
                                    <span class="text-white float-sm-left ">{{$remises->prix}} da</span>
                                   <span class="badge ml-2  mr-1 float-sm-left  badge-info"> - {{$percentage}} % </span>
-                                  <span class="badge ml-2  mr-1 float-sm-right text-white  badge-success">  {{$commande->total_price}} Da  </span>
+                                  <span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">  {{$commande->total_price}} Da  </span>
 
                                 </div>
 
