911120ea903a60d57fe669529a7e651e59385c32
1 <abdounovich@gmail.com>
2021-02-06 15:53:16 +0100

    'comment'

--- resources/views/commandes/annuler_par_admin.blade.php
+++ resources/views/commandes/annuler_par_admin.blade.php
@@ -1,4 +1,8 @@
-
+<style>
+    .border-orange{
+        border-color:darkred;
+    }
+</style>
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-dark rounded" style="opacity: 0.9"> 
@@ -11,16 +15,16 @@
 
     @foreach ($annuler_par_admin as $commande)
         @if ($commande->commande_type=="color")
-        <div style="border-width: 10px" class="card bg-dark border-danger     mt-2 mb-1" >
+        <div style="border-width: 10px" class="card bg-dark border-orange     mt-2 mb-1" >
             @include('commandes.color')
         </div>
 
         @elseif($commande->commande_type=="taille")
-        <div style="border-width: 10px" class="card bg-dark border-danger     mt-2 mb-1" >
+        <div style="border-width: 10px" class="card bg-dark border-orange     mt-2 mb-1" >
             @include('commandes.taille')
         </div>
         @elseif($commande->commande_type=="simple")
-        <div style="border-width: 10px" class="card bg-dark border-danger     mt-2 mb-1" >
+        <div style="border-width: 10px" class="card bg-dark border-orange     mt-2 mb-1" >
             @include('commandes.simple')
         </div>
         @endif
