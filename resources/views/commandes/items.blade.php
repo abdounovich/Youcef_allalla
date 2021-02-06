4a3e9ac10acc2915849295070e55d2026eb169ee
1 <abdounovich@gmail.com>
2021-02-06 22:26:41 +0100

    'comment'

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -28,7 +28,7 @@
                             @else 
                             <div class="mt-2" ><span class="  text-secondary">{{$commande->created_at->format("d-m-y H:i")}}</span></div>
    <div class="card-text  text-white mt-1  h5"> {{$commande->product->prix*$commande->quantity}} da
-    <span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span><span class="text-white float-right">Total:</span>
+    <span class="badge  float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span><span class="text-white float-right">Total:</span>
  
    </div>
    
