bced627503ecddd25f2259ba786b37e255a929d2
1 <abdounovich@gmail.com>
2021-02-06 15:42:42 +0100

    'comment'

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -4,7 +4,7 @@
    
    <div class="mt-2" ><span class="  text-secondary">{{$commande->created_at->format("d-m-y H:i")}}</span></div>
    <div class="card-text  text-white mt-1  h5"> {{$commande->total_price}} da 
-    <span class="text-white float-right">Total:</span><span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span>
+    <span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span><span class="text-white float-right">Total:</span>
 
     
    </div>
@@ -20,7 +20,7 @@
                                  <div class="card-text h5 clearfix  ">
                                    <span class="text-white float-sm-left ">{{$remises->prix}} da</span>
                                   <span class="badge ml-2  mr-1 float-sm-left  badge-info"> - {{$percentage}} % </span>
-                                  <span class="text-white float-right">Total:</span><span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span>
+                                  <span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span><span class="text-white float-right">Total:</span>
 
                                 </div>
 
@@ -28,7 +28,7 @@
                             @else 
                             <div class="mt-2" ><span class="  text-secondary">{{$commande->created_at->format("d-m-y H:i")}}</span></div>
    <div class="card-text  text-white mt-1  h5"> {{$commande->product->prix*$commande->quantity}} da
-    <span class="text-white float-right">Total:</span><span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span>
+    <span class="badge ml-2 float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span><span class="text-white float-right">Total:</span>
  
    </div>
    
