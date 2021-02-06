61de584da04f7e220da382369dd9c130fc036b1a
1 <abdounovich@gmail.com>
2021-02-06 22:38:36 +0100

    'comment'

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -1,9 +1,10 @@
 @php
-   $remises=App\Remise::where("product_id",$commande->product->id)->first();@endphp
+   $remises=App\Remise::where("product_id",$commande->product->id)->first();
+   @endphp
    @if (!$remises) 
    
    <div class="mt-2" ><span class="  text-secondary">{{$commande->created_at->format("d-m-y H:i")}}</span></div>
-   <div class="card-text  text-white mt-1  h5"> {{$commande->product->prix}} dz 
+   <div class="card-text  text-white mt-1  h1"> {{$commande->product->prix}} daazzz
     <span class="badge float-right  mr-1 float-sm-right text-white  badge-success">   {{$commande->total_price}} Da  </span>
 
     
