f379dabf5e4e97625efa20b6073d8998a80c1968
1 <abdounovich@gmail.com>
2021-02-13 14:09:33 +0100

    comment

--- resources/views/commandes/complexe.blade.php
+++ resources/views/commandes/complexe.blade.php
@@ -29,7 +29,8 @@
   <span class=" fa fa-trash  "></span>
 </a></p>
 
-    <p class=" text-white text-wrap mt-2 " >couleur: <span class="text-info">{{$taille->color->couleur}}</span> Taille: <span class="text-info>{{$taille->taille}}</span>  </p>
+    <p class=" text-white text-wrap mt-2 " >couleur: <span class="text-info">{{$taille->color->couleur}}</span></p>
+    <p> Taille: <span class="text-info">{{$taille->taille}}</span>  </p>
 
 
        <img class="img-thumbnail custom  p-0 mt-2 " style="width: 100%;height:250px" src="{{$commande->product->photo}}" alt="">
