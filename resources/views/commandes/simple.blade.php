5e53ea677fb02404f444a916f09d6f4cf1d17c2a
1 <abdounovich@gmail.com>
2021-02-13 15:36:05 +0100

    comment

--- resources/views/commandes/simple.blade.php
+++ resources/views/commandes/simple.blade.php
@@ -10,7 +10,7 @@
       <button class="btn btn-dark " type="button" id="dropdownMenuButton" data-toggle="dropdown" >
        <i class="fa mr-0 fa-2x text-bold fa-ellipsis-v"></i>
       </button>
-      <div class="dropdown-menu " aria-labelledby="dropdownMenuButton">
+      <div class="dropdown-menu ml-5 " aria-labelledby="dropdownMenuButton">
         <a class="text-dark p-2 m-2"  href="{{route('commandes.delete',$commande->id)}}"  >
 effacer</a>        </div>
     </div>
