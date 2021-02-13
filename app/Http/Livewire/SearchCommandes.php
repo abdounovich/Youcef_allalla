7ee5d2cc7f8dfec9647c12aef01b8d7e17c3613b
1 <abdounovich@gmail.com>
2021-02-13 19:19:06 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -2,48 +2,12 @@
 
 namespace App\Http\Livewire;
 
-use App\Commande;
 use Livewire\Component;
 
 class SearchCommandes extends Component
-{ public $message="";
-    public $type;
-    public $query;
-    public $inactive_commandes;
-    public $active_commandes;
-
+{
     public function render()
     {
-        $this->inactive_commandes=Commande::where('id','like',"%{$this->query}%")->get();
-        if ($this->inactive_commandes->count()=="0") {
-          $this->message="pas de résultat";
-        }
-        else {
-         $this->message="";
- 
-        }
-
-        $this->active_commandes=Commande::where('id','like',"%{$this->query}%")->get();
-        if ($this->active_commandes->count()=="0") {
-          $this->message="pas de résultat";
-        }
-        else {
-         $this->message="";
- 
-        }
         return view('livewire.search-commandes');
-    }
-
-
-    public function mount()
-    {  $this->inactive_commandes=Commande::where("type",1)->paginate(10);
-        $this->active_commandes=Commande::where("type",2)->paginate(10);
-        $this->delivré_commandes=Commande::where("type",3)->paginate(10);
-        $this->enroute_commandes=Commande::where("type",6)->paginate(10);
-        $this->annuler_par_admin=Commande::where("type",4)->paginate(10);
-        $this->annuler_par_client=Commande::where("type",5)->paginate(10);
-
-        return view('livewire.search-commandes');
-
     }
 }
--- resources/views/commandes/non_confirmed.blade.php
+++ resources/views/commandes/non_confirmed.blade.php
@@ -2,22 +2,30 @@
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-secondary rounded" style="opacity: 0.9"> 
+        Non-confirmées :  {{$inactive_commandes_count}}
     </h1>
     </div>
 </div>
  
 
-@livewire('search-commandes')
+
     @foreach ($inactive_commandes as $commande)
+       
         <div style="border-width: 10px" class="card bg-dark border-secondary     mt-2 mb-1" >
             @include('commandes.simple')
         </div>
+
+    
+
+
     @endforeach
     
+
     <br>
+
     <div class="d-flex justify-content-center m-4">
-{{--         {{$inactive_commandes->links()}}
- --}}    </div>
+        {{$inactive_commandes->links()}}
+    </div>
   
   
   
