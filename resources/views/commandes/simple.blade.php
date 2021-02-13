1077a40a012db70184437b806ca6e5880de1d145
1 <abdounovich@gmail.com>
2021-02-13 14:23:16 +0100

    comment

--- resources/views/commandes/simple.blade.php
+++ resources/views/commandes/simple.blade.php
@@ -24,7 +24,7 @@
 
        <div class="dropdown">
         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown button
+         <i class="fa fa-ellipsis-v"></i>
         </button>
         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <a class="dropdown-item" href="#">Action</a>
