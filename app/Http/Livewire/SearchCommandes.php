2e7577c2744a4762cece4602ed3a919616f56f39
1 <abdounovich@gmail.com>
2021-02-14 20:38:10 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use App\Commande;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Database\Query\Builder;
+
 class SearchCommandes extends Component
 {
     public $message="";
@@ -12,7 +14,19 @@ class SearchCommandes extends Component
     public $query="";
     public function render()
     {
-        $this->commandes=Commande::where('slug','ILIKE','%'.$this->query.'%')->get();
+        $this->commandes=Commande::where('slug','ILIKE','%'.$this->query.'%')
+        ->orWhere('slug','ILIKE','%'.$this->query.'%')
+        ->orWhere('type','ILIKE','%'.$this->query.'%')
+        ->orWhere('color','ILIKE','%'.$this->query.'%')
+        ->orWhere('commande_type','ILIKE','%'.$this->query.'%')
+
+        ->orWhere('taille','ILIKE','%'.$this->query.'%')
+
+        ->orWhere('total_price','ILIKE','%'.$this->query.'%')
+
+->orWhereHas('product', function (Builder $req) {
+            $req->where('nom', 'ILIKE', '%'.$this->query.'%');
+        })->get();
         
         return view('livewire.search-commandes');
     }
