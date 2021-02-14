dc4c9e15a9ff4c6c7c09c5d526e4f79be8bcd2e8
1 <abdounovich@gmail.com>
2021-02-14 19:47:58 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -13,15 +13,7 @@ class SearchCommandes extends Component
 
     public function render()
     {
-        $this->commandes=Commande::where('type','LIKE','%'.$this->query.'%')->get();
-
-        if ($this->commandes->count()=="0") {
-            $this->message="pas de résultat";
-          }
-          else {
-           $this->message="";
-   
-          }
+        
         return view('livewire.search-commandes');
     }
 
