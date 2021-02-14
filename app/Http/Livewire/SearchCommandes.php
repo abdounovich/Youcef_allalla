43f21164ca75bb216d3a682693468271ef10cb06
1 <abdounovich@gmail.com>
2021-02-14 18:57:39 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Livewire;
 
+use App\Commande;
 use Livewire\Component;
 
 class SearchCommandes extends Component
 {
+    public $query="";    
+    public $commandes="";
+    public $message="";
+
     public function render()
     {
+        $this->commandes=Commande::where('type','ILIKE','%'.$this->query.'%')->get();
+
+        if ($this->commandes->count()=="0") {
+            $this->message="pas de résultat";
+          }
+          else {
+           $this->message="";
+   
+          }
+        return view('livewire.search-commandes');
+    }
+
+
+    public function mount()
+    {
+
+        $this->commandes=Commande::all();
+
+      
         return view('livewire.search-commandes');
     }
 }
