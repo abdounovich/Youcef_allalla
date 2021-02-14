64b0b792a9cd0a4060e31de5284ce69550f29799
1 <abdounovich@gmail.com>
2021-02-14 18:42:29 +0100

    comment

--- resources/views/commandes/non_confirmed.blade.php
+++ resources/views/commandes/non_confirmed.blade.php
@@ -17,10 +17,11 @@
         <div style="border-width: 10px" class="card bg-dark border-secondary     mt-2 mb-1" >
             @include('commandes.simple')
         </div>
+        @endIf
     @endforeach
     @foreach ($commandes as $commande)
 
-@elseIf($commande->type=="2")
+@If($commande->type=="2")
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-primary rounded" style="opacity: 0.9"> 
@@ -31,9 +32,10 @@
 <div style="border-width: 10px" class="card bg-dark border-primary     mt-2 mb-1" >
     @include('commandes.simple')
 </div>
+@endIf
 @endforeach
 @foreach ($commandes as $commande)
-@elseIf($commande->type=="6")
+@If($commande->type=="6")
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-warning rounded" style="opacity: 0.9"> 
@@ -44,9 +46,10 @@
 <div style="border-width: 10px" class="card bg-dark border-warning     mt-2 mb-1" >
     @include('commandes.simple')
 </div>
+@endIf
 @endforeach
 @foreach ($commandes as $commande)
-@elseIf($commande->type=="3")
+@If($commande->type=="3")
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-success rounded" style="opacity: 0.9"> 
@@ -57,9 +60,11 @@
 <div style="border-width: 10px" class="card bg-dark border-success     mt-2 mb-1" >
     @include('commandes.simple')
 </div>
+@endIf
+
 @endforeach
 @foreach ($commandes as $commande)
-@elseIf($commande->type=="4")
+@If($commande->type=="4")
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-oranger rounded" style="opacity: 0.9"> 
@@ -70,9 +75,11 @@
 <div style="border-width: 10px" class="card bg-dark border-oranger     mt-2 mb-1" >
     @include('commandes.simple')
 </div>
+@endIf
+
 @endforeach
 @foreach ($commandes as $commande)
-@elseIf($commande->type=="5")
+@If($commande->type=="5")
 <div class="row text-center text-white mb-3">
     <div class="col-lg-7 mx-auto">
     <h1 class=" h4 mt-4 p-3 shadow bg-danger rounded" style="opacity: 0.9"> 
