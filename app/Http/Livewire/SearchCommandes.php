77225641f2dbabe9133e60607e14c0834078621f
1 <abdounovich@gmail.com>
2021-02-15 22:26:01 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -35,7 +35,7 @@ class SearchCommandes extends Component
             })->get();
 
         }  */
-$commandes="";
+
         if($this->categorie=="slug" OR $this->categorie=="type" OR $this->categorie=="total_price"){
         $commandes=Commande::where($this->categorie,'LIKE','%'.$this->query.'%')->paginate(10);}
 
@@ -52,10 +52,10 @@ $commandes="";
         return view('livewire.search-commandes',["commandes"=>$commandes]);
     }
 
-/* public function mount(){
+ public function mount(){
     $commandes=Commande::paginate(10);
     return view('livewire.search-commandes',["commandes"=>$commandes]);
 
-} */
+} 
 
 }
