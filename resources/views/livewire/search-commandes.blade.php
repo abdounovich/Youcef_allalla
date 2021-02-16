a41fa65dd19d38f4028592347da3e35c70fbe9b2
1 <abdounovich@gmail.com>
2021-02-16 19:17:44 +0100

    comment

--- resources/views/livewire/search-commandes.blade.php
+++ resources/views/livewire/search-commandes.blade.php
@@ -1,8 +1,7 @@
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.js" integrity="sha512-WNLxfP/8cVYL9sj8Jnp6et0BkubLP31jhTG9vhL/F5uEZmg5wEzKoXp1kJslzPQWwPT1eyMiSxlKCgzHLOTOTQ==" crossorigin="anonymous"></script>  <div class="form-check form-check-inline">
-        <input class="form-check-input" type="radio" wire:model="categorie" value="slug" name="categorie" id="inlineRadio1" >
+
+<input class="form-check-input" type="radio" wire:model="categorie" value="slug" name="categorie" id="inlineRadio1" >
         <label class="form-check-label" for="inlineRadio1">Code</label>
       </div>
       <div class="form-check form-check-inline">
