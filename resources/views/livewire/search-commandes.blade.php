52a651d8ee572d8b4b32fcf285a5a8c7ff1f85b2
1 <abdounovich@gmail.com>
2021-02-16 18:13:24 +0100

    comment

--- resources/views/livewire/search-commandes.blade.php
+++ resources/views/livewire/search-commandes.blade.php
@@ -1,6 +1,6 @@
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>
+{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script> --}}
 {{-- <div>
   --}}
 
