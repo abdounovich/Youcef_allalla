4461c8aaa8db34cc4ef225d3577db016d96d2e88
1 <abdounovich@gmail.com>
2021-02-17 21:51:59 +0100

    comment

--- resources/views/livewire/search-commandes.blade.php
+++ resources/views/livewire/search-commandes.blade.php
@@ -5,7 +5,7 @@
 
     <p>
         <div class="row">
-        <a class="btn btn-primary col col-4" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+        <a class="btn btn-primary col col-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
              <i class="fa fa-filter"></i> Filtrer        </a>
              <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-6 ml-2" placeholder="Entrer le nom du produit">
         </div>
