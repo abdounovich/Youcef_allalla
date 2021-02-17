d6ecbb73351610e1935403aebff1083950b2b98b
1 <abdounovich@gmail.com>
2021-02-17 20:06:50 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -28,33 +28,39 @@ class SearchCommandes extends Component
    
     public function render()
     {
-     
+       $this->commandes=Commande::where($this->categorie,'ILIKE','%'.$this->query.'%')
+       ->orderBy('created_at', 'desc')->get()
+       ->take($this->TakeLimit);
 
        if ($this->commandes->count()=="0") {
-          $this->message="Pas de resultat pour ".$this->query;
+          $this->message="0 resultat pour ".$this->query;
        }
-else
-       $this->commandes=Commande::whereHas('product', function (Builder $req) {
-        $req->where($this->categorie, 'ILIKE', '%'.$this->query.'%');
-    })->get()->take($this->TakeLimit);
-       
+       else{
+        $this->message="";
+
+
+       }
+
+
 
          if($this->categorie=="slug" OR $this->categorie=="type" OR $this->categorie=="total_price"){
-        $this->commandes=Commande::where($this->categorie,'ILIKE','%'.$this->query.'%')->get();}
+        $this->commandes=Commande::where($this->categorie,'ILIKE','%'.$this->query.'%')->get()->take($this->TakeLimit);}
 
-         elseif ($this->categorie=="facebook" OR $this->categorie=="wilaya" ){
+        elseif ($this->categorie=="facebook" OR $this->categorie=="wilaya" ){
            $this->commandes=Commande::whereHas('client', function (Builder $req) {
                 $req->where($this->categorie, 'ILIKE', '%'.$this->query.'%');
-            })->get();}
+            })->get()->take($this->TakeLimit);}
             elseif ($this->categorie=="nom" ){
                $this->commandes=Commande::whereHas('product', function (Builder $req) {
                     $req->where($this->categorie, 'ILIKE', '%'.$this->query.'%');
-                })->get();} else {
-                    $this->commandes=Commande::where($this->categorie,'ILIKE','%'.$this->query.'%')
-                    ->orderBy('created_at', 'desc')->get()
-                    ->take($this->TakeLimit);
-                }
+                })->get()->take($this->TakeLimit);}
 
+                else {
+                    $this->commandes=Commande::whereHas('product', function (Builder $req) {
+                        $req->where($this->categorie, 'ILIKE', '%'.$this->query.'%');
+                    })->get()->take($this->TakeLimit);
+           
+                }
         return view('livewire.search-commandes');
     }
 
