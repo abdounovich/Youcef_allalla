3e8981734f66e18f1d05f2ac2978131f14c52565
1 <abdounovich@gmail.com>
2021-02-18 18:28:04 +0100

    comment

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -4,33 +4,51 @@ namespace App\Http\Livewire;
 
 use App\Product;
 use Livewire\Component;
-use Livewire\WithPagination;
 
 
 class Search extends Component
 {
-use WithPagination;
-    public $query="";    
-    public $message="";
-    public $statue="0";
-    protected $paginationTheme = 'bootstrap';
+    public $query="";
+    public $produits="";
+    public $categorie="";
+    public $TakeLimit="5";
+    public $activation="1";
+    public $type="";
 
-
-    public function change($statue)
-    {
-        $this->statue=$statue;
-    }
-    public function render()
-    {
-      
-       
-            return view('livewire.search',["produits"=>Product::where('nom','like',"%{$this->query}%")->paginate(10)
-            ]);
-
+    public function loadMore()
+     {   $this->TakeLimit=$this->TakeLimit+5;
        
     }
+   
+    public function change($value){
+        $this->categorie=$value;
+    }
 
-
+    public function changetype($value){
+        $this->categorie='type';
+        $this->type=$value;
+    }
 
     
+   
+    public function render()
+    { $this->produits=Product::where($this->categorie,'ILIKE','%'.$this->query.'%')->get()->take($this->TakeLimit);
+        if ( $this->categorie=='type') {
+             $this->produits=Product::where($this->categorie,'ILIKE','%'.$this->type.'%')->get()->take($this->TakeLimit);
+
+        }
+        else {
+            $this->produits=Product::all()
+            ->take($this->TakeLimit);
+        }
+    }
+   public function mount(){
+    $this->produits=Product::orderBy('created_at', 'desc')->get()
+    ->take($this->TakeLimit);
+
+    
+    return view('livewire.search');
+
+}  
+ 
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,137 +1,221 @@
+
 <div>
 
-    <div class="input-group mb-3">
-        <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control" placeholder="Entrer le nom du produit">
       
+  
+
+  <p>
+      <div class="row ml-0">
+      <a class="btn btn-primary col col-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+           <i class="fa fa-filter"></i> Filtrer        </a>
+           <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-8 ml-3" 
+           placeholder="Rechercher">
       </div>
-{{$message}}
-@if ($message=="")
-    
-  <div class="row text-center text-white ">
-        <div class="col-lg-7 mx-auto">
-            <h1 class="h4 shadow bg-dark p-3" style="opacity: 0.9">Liste des produits</h1>
-        </div>
+    @if ($commandes->count()=="0")
+  <p class="mt-3 ml-2 text-danger h5">Pas de résultat pour : {{$query}}</p>
+  @endif
+
+    </p>
+    <div class="collapse" id="collapseExample">
+      <button class="btn btn-danger text-left "   wire:click="change('all')"  name="categorie" id="inlineRadio1"> <i class="fa fa-times-circle"></i> effacer filtre</button>
+      <hr>
+      <div class="card card-body">
+          
+  
+<div class="row">
+  
+<button class="btn btn-link "   wire:click="change('nom')"  name="categorie" id="inlineRadio1">nom du produit</button>
+<button  class="btn btn-link "    wire:click="change('taille')"  name="categorie" id="inlineRadio1">Taille</button>
+<button  class="btn btn-link"    wire:click="change('couleur')"  name="categorie" id="inlineRadio1">Couleur</button>
+<button  class="btn btn-link"    wire:click="change('prix')"  name="categorie" id="inlineRadio1">Prix </button>
+<div class="col col-10 ml-4">
+      <input class="form-check-input " type="radio" wire:click="changetype('simple')" name="categorie" id="inlineRadio1" >
+          <label class="form-check-label ml-1" for="inlineRadio1">Simple</label>
+          <br>
+          <input class="form-check-input " type="radio" wire:click="changetype('taille')" name="categorie" id="inlineRadio2" >
+          <label class="form-check-label ml-1" for="inlineRadio2">Taille</label>
+          <br>
+          <input class="form-check-input " type="radio" wire:click="changetype('color')" name="categorie" id="inlineRadio1" >
+          <label class="form-check-label ml-1" for="inlineRadio1">Couleur</label> 
+          <br>
+          <input class="form-check-input " type="radio" wire:click="changetype('complexe')" name="categorie" id="inlineRadio1" >
+          <label class="form-check-label ml-1" for="inlineRadio1">Couleur + taille</label>
+     
+          </div>
+</div>
+      
+       
+
+
+
+      </div>
     </div>
 
-@endif
 
 
-<div class="  mt-3 mb-2 d-flex justify-content-center ">
-  <a class="btn btn-info  rounded p-3 m-4 text-white"  
-  type="button" data-toggle="collapse" data-target="#List_collapse" aria-expanded="false"><i class="fa fa-plus mr-2">  </i> Ajouter un produit</a>
-
-</div>
 
 
-    @foreach ($produits as $produit)
-    
-<div class="card bg-dark text-white p-2 mt-5 shadow-lg" >
-  <img class="card-img-top img-thumbnail p-1" style="width: 100%; height:300px" src="{{$produit->photo}}" alt="Card image">
-  <div class="card-body">
-    <h4 class="card-title">{{$produit->nom}}</h4>
+
+  <style>
+      .text-gray {
+         color: #aaa
+     }
+     
+     img {
+         height: 170px;
+         width: 140px
+     }
+     .btn-circle {
+       width: 30px;
+       height: 30px;
+       text-align: center;
+       padding: 2px;
+       font-size: 18px;
+       border-radius: 15px;
+     }
+     img {
+         height: 170px;
+         width: 140px
+     }
+     .bg-c-blue {
+         background: linear-gradient(45deg,#3a96cc,#0176fc);
+     }
+     
+     .custom{
+     
+         width: 50px;
+         height: 50px;
+     
+     }
+     </style>
+     
+     
+   
   
-    <hr>
-    <p>{{$produit->SubCategories->nom}} -> {{$produit->SubCategories->categories->nom}}</p>
-    <p class="card-text">{{$produit->descreption}}</p>
-    @if ($produit->product_type=="taille")
-
-    <div class="row">
-      @foreach ($produit->taille as $taille)
+ 
+     <div class="  mt-3 mb-2 d-flex justify-content-center ">
+      <a class="btn btn-info  rounded p-3 m-4 text-white"  
+      type="button" data-toggle="collapse" data-target="#List_collapse" aria-expanded="false"><i class="fa fa-plus mr-2">  </i> Ajouter un produit</a>
+    
+    </div>
+    
+    
+        @foreach ($produits as $produit)
+        
+    <div class="card bg-dark text-white p-2 mt-5 shadow-lg" >
+      <img class="card-img-top img-thumbnail p-1" style="width: 100%; height:300px" src="{{$produit->photo}}" alt="Card image">
+      <div class="card-body">
+        <h4 class="card-title">{{$produit->nom}}</h4>
+      
+        <hr>
+        <p>{{$produit->SubCategories->nom}} -> {{$produit->SubCategories->categories->nom}}</p>
+        <p class="card-text">{{$produit->descreption}}</p>
+        @if ($produit->product_type=="taille")
+    
+        <div class="row">
+          @foreach ($produit->taille as $taille)
+              <div class=" col-4 p-2">
+              <p class="card-text  h5  ">{{$taille->taille}} :  
+             <span class="ml-1 text-success">{{$taille->quantity}}</span> </p> 
+            </div>
+              @endforeach
+              </div>
+        @elseIf ($produit->product_type=="color")
+        <div class="row">
+          @foreach ($produit->color as $color)
+                  
+            
+              <div class=" col-4 p-2">
+              <img class=" img-thumbnail " src="{{$color->photo}}" style="width: 50px; height:50px" alt=""><p></p>
+              <p class="card-text  h5  ">{{$color->couleur}} :  </p>
+              <p class="card-text  text-success h5 ">{{$color->quantity}} </p> 
+            </div>
+              @endforeach
+              </div>
+        @endif
+       
+        <hr>
+      @php
+          $remises=App\Remise::where("product_id",$produit->id)->first();
+          if (!$remises) {
+    echo"<p class='card-text text-info h5'>prix : ".$produit->prix."da</p>";
+    }else {
+    $percentage=round(100-$remises->prix*100/$remises->produit->prix);
+    
+    
+    
+    echo '   <p class="card-text text-info h5  "> <del class="text-danger">'.$produit->prix.' da  </del> 
+          <span class="text-success ml-3">'.$remises->prix.' da</span>                      
+          <span class="badge p-1 ml-3 badge-info"> - '.$percentage.' % </span>
+        </p>';
+    
+    }
+    
+      @endphp
+        <hr>
+    
+        @if ($produit->product_type=="complexe")
+           
+         <div class="row">
+          @foreach ($produit->color as $color)
+                      
+                
           <div class=" col-4 p-2">
+                <p class="card-text  h5  ">{{$color->couleur}} </p>
+    <img class=" img-thumbnail " src="{{$color->photo}}" style="width: 50px; height:50px" alt=""><p></p>
+          @foreach ($color->taille as $taille)
+              
+        
+          <div class=" p-2">
           <p class="card-text  h5  ">{{$taille->taille}} :  
          <span class="ml-1 text-success">{{$taille->quantity}}</span> </p> 
         </div>
           @endforeach
-          </div>
-    @elseIf ($produit->product_type=="color")
-    <div class="row">
-      @foreach ($produit->color as $color)
-              
-        
-          <div class=" col-4 p-2">
-          <img class=" img-thumbnail " src="{{$color->photo}}" style="width: 50px; height:50px" alt=""><p></p>
-          <p class="card-text  h5  ">{{$color->couleur}} :  </p>
-          <p class="card-text  text-success h5 ">{{$color->quantity}} </p> 
         </div>
-          @endforeach
-          </div>
-    @endif
-   
-    <hr>
-  @php
-      $remises=App\Remise::where("product_id",$produit->id)->first();
-      if (!$remises) {
-echo"<p class='card-text text-info h5'>prix : ".$produit->prix."da</p>";
-}else {
-$percentage=round(100-$remises->prix*100/$remises->produit->prix);
-
-
-
-echo '   <p class="card-text text-info h5  "> <del class="text-danger">'.$produit->prix.' da  </del> 
-      <span class="text-success ml-3">'.$remises->prix.' da</span>                      
-      <span class="badge p-1 ml-3 badge-info"> - '.$percentage.' % </span>
-    </p>';
-
-}
-
-  @endphp
-    <hr>
-
-    @if ($produit->product_type=="complexe")
+      
+        
+          @endforeach  
        
-     <div class="row">
-      @foreach ($produit->color as $color)
-                  
-            
-      <div class=" col-4 p-2">
-            <p class="card-text  h5  ">{{$color->couleur}} </p>
-<img class=" img-thumbnail " src="{{$color->photo}}" style="width: 50px; height:50px" alt=""><p></p>
-      @foreach ($color->taille as $taille)
-          
+      </div> 
+        @endif
+        @if ($produit->product_type=="simple")
+        <p class="card-text h5">reste : <span class="text-success">{{$produit->quantity}}</span>  
+        @endif
+          <a class="btn btn-warning btn-circle float-right  mr-2  " href="{{route('products.edit',$produit->id) }}">
+            <span class="text-white  fa fa-edit   "></span>
+        </a>
+        @if ($produit->product_type=="complexe")
     
-      <div class=" p-2">
-      <p class="card-text  h5  ">{{$taille->taille}} :  
-     <span class="ml-1 text-success">{{$taille->quantity}}</span> </p> 
+        <a class="btn btn-success btn-circle float-right  mr-2 " href="{{route('products.steps',['1',$produit->id])}}">
+          <span class=" text-white fa fa-plus   "></span>
+        </a>
+        @endif
+    
+        <a class="btn btn-info btn-circle float-right  mr-2  " href="{{route('remises.addByIdShow',$produit->id) }}">
+          <span class="text-white  fa fa-level-down   "></span>
+      </a>
+        <a class="btn btn-danger btn-circle float-right  mr-2 " href="{{route('products.delete',$produit->id)}}">
+            <span class=" text-white  fa fa-trash  "></span>
+        </a></p>
+    
+    
+      </div>
     </div>
-      @endforeach
-    </div>
+               
+                 @endforeach
   
-    
-      @endforeach  
-   
-  </div> 
-    @endif
-    @if ($produit->product_type=="simple")
-    <p class="card-text h5">reste : <span class="text-success">{{$produit->quantity}}</span>  
-    @endif
-      <a class="btn btn-warning btn-circle float-right  mr-2  " href="{{route('products.edit',$produit->id) }}">
-        <span class="text-white  fa fa-edit   "></span>
-    </a>
-    @if ($produit->product_type=="complexe")
 
-    <a class="btn btn-success btn-circle float-right  mr-2 " href="{{route('products.steps',['1',$produit->id])}}">
-      <span class=" text-white fa fa-plus   "></span>
-    </a>
-    @endif
-
-    <a class="btn btn-info btn-circle float-right  mr-2  " href="{{route('remises.addByIdShow',$produit->id) }}">
-      <span class="text-white  fa fa-level-down   "></span>
-  </a>
-    <a class="btn btn-danger btn-circle float-right  mr-2 " href="{{route('products.delete',$produit->id)}}">
-        <span class=" text-white  fa fa-trash  "></span>
-    </a></p>
-
-
-  </div>
-</div>
-           
-             @endforeach
+  <div class="row text-center text-white my-5">
+      <div class="col-lg-7 mx-auto">
+          <button  class="btn btn-primary" @if ($TakeLimit>$produits->count()) 
+              disabled class='btn btn-danger'    @endif  wire:click="loadMore" type="button">Afficher plus</button>
+      </div>
+      </div> 
+  
 
 </div>
 
-<div class=" container">
-  <div  class="row d-flex mt-2 justify-content-center">
-  <div  >{{$produits->links()}}
- </div>
-</div> 
-</div>
+
+
+
+
