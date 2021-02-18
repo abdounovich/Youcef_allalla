07c148b131312d1c58eb1c836032f6c04e347c4f
1 <abdounovich@gmail.com>
2021-02-18 21:54:00 +0100

    comment

--- resources/views/livewire/search-client.blade.php
+++ resources/views/livewire/search-client.blade.php
@@ -10,7 +10,7 @@
     <p>
         <div class="row ml-0">
         <a class="btn btn-primary col col-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-             <i class="fa fa-filter"></i> Filtrer        </a>
+             <i class="fa fa-filter"></i>         </a>
              <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-8 ml-3" 
              placeholder="Rechercher">
         </div>
