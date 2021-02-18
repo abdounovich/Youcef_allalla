574e34b3d86aab91a2cd56ee7bba7810df8ed3e9
1 <abdounovich@gmail.com>
2021-02-18 22:21:06 +0100

    comment

--- resources/views/livewire/search-client.blade.php
+++ resources/views/livewire/search-client.blade.php
@@ -5,14 +5,14 @@
 
       
   
-<div>
    
     <p>
-        <div class="row">
-        <a class="btn btn-primary col col-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-             <i class="fa fa-filter"></i> Filtrer</a>
-            
-        </div>
+        <div class="row ml-0">
+            <a class="btn btn-primary col col-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                 <i class="fa fa-filter"></i> Filtrer        </a>
+                 <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-8 ml-3" 
+                 placeholder="Rechercher">
+            </div>
       @if ($clients->count()=="0")
     <p class="mt-3 ml-2 text-danger h5">Pas de résultat pour : {{$query}}</p>
     @endif
@@ -186,4 +186,3 @@
   
 
 
-</div>
