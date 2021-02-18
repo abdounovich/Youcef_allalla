1c3f49881d3f3330db3bed7115a0064f8653148f
1 <abdounovich@gmail.com>
2021-02-18 22:17:48 +0100

    comment

--- resources/views/livewire/search-client.blade.php
+++ resources/views/livewire/search-client.blade.php
@@ -11,8 +11,7 @@
         <div class="row">
         <a class="btn btn-primary col col-3" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
              <i class="fa fa-filter"></i> Filtrer</a>
-             <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-6 ml-1" 
-             placeholder="Rechercher">
+            
         </div>
       @if ($clients->count()=="0")
     <p class="mt-3 ml-2 text-danger h5">Pas de résultat pour : {{$query}}</p>
