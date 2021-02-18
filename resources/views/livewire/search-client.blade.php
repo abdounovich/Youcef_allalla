23c44c71dad2ebe3e11f78bd71f91fe0cb875ca5
1 <abdounovich@gmail.com>
2021-02-18 21:43:22 +0100

    comment

--- resources/views/livewire/search-client.blade.php
+++ resources/views/livewire/search-client.blade.php
@@ -10,7 +10,7 @@
         <div class="row  my-2">
         <a class="btn btn-primary col col-4" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
              <i class="fa fa-filter"></i> Filtrer        </a>
-             <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-8 ml-3" 
+             <input type="search" wire:model.bounce.500ms="query" name="query" class="form-control col col-7 ml-3" 
              placeholder="Rechercher">
         </div>
       @if ($clients->count()=="0")
