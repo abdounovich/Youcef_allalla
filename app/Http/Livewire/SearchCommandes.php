ec1f75104333c9589529346a323f341c220f00d0
1 <abdounovich@gmail.com>
2021-02-19 23:46:04 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -10,7 +10,7 @@ class SearchCommandes extends Component
 {
     public $query="";
     public $commandes="";
-    public $categorie="";
+    public $categorie="nom";
     public $TakeLimit="5";
     public $activation="1";
     public $type="1";
