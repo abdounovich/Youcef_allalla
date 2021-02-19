576ff87f799cc3565f31c28869864cd185e537db
1 <abdounovich@gmail.com>
2021-02-19 11:19:26 +0100

    comment

--- app/Http/Livewire/SearchCommandes.php
+++ app/Http/Livewire/SearchCommandes.php
@@ -13,9 +13,9 @@ class SearchCommandes extends Component
     public $categorie="";
     public $TakeLimit="5";
     public $activation="1";
-    public $type="2";
+    public $type="";
     public $total='0';
-    public $trierPar="type";
+    public $trierPar="";
     protected $listeners = [
         'loadMore' => 'loadMore'
     ];
