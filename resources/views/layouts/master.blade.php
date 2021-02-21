c7ae1e1932a729ef23820f696a3e70b85099cca3
1 <abdounovich@gmail.com>
2021-02-21 21:22:43 +0100

    comment

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -66,10 +66,10 @@
 
     @livewireScripts
     <div class="d-flex fixed-bottom flex-center bg-info mb-0">
-      <div class="p-3 btn flex-fill "><a href="/"><i  @if(request()->route('/') ) active @endif class="text-white fa  fa-home"></i></a></div>
+      <div class="p-3 bg-white  btn flex-fill"><a href="/"><i class="text-info fa   fa-home"></i></a></div>
+
       <div class="p-3 btn flex-fill "><a href="/"><i class="text-white fa  fa-users"></i></a></div>
       <div class="p-3 btn flex-fill"><a href="/"><i class="text-white fa   fa-shopping-cart"></i></a></div>
-      <div class="p-3 btn flex-fill"><a href="/"><i class="text-white fa   fa-list-alt"></i></a></div>
       <div class="p-3 btn flex-fill"><a href="/"><i class="text-white fa   fa-list"></i></a></div>
     
     
