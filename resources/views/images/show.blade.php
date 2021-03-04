e90e6830d6be8d497445d61a12dbbfad424ecff3
1 <abdounovich@gmail.com>
2021-03-04 20:49:59 +0100

    comment

--- resources/views/images/show.blade.php
+++ resources/views/images/show.blade.php
@@ -116,61 +116,7 @@ img {vertical-align: middle;}
 </head>
 <body>
 
-<div class="slideshow-container">
-@foreach ($images as $image)
-    
-<div class="mySlides fade">
-  <div class="numbertext" >{{$loop->index+1}}
-    / {{$images->count()}}</div>
-  <img src="{{$image->image}}"  style="width:200px ; height:280px; margin:5px ; display: block;
-  margin-left: auto;
-  margin-right: auto ">
-  <div class="text" style="color: goldenrod">{{$image->product->nom}}</div>
-</div>
-@endforeach
 
-
-<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-<a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-</div>
-<br>
-
-<div style="text-align:center">
-    @foreach ($images as $image)
-
-  <span class="dot" onclick="currentSlide({{$loop->index+1}})"></span> 
-@endforeach
-</div>
-
-<script>
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-</script>
 
 </body>
 </html> 
