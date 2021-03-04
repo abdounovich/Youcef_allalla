812f8465966d35c2a72785176d117f659638dc68
1 <abdounovich@gmail.com>
2021-03-04 21:52:18 +0100

    comment

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -199,8 +199,64 @@ img {
           
       @else
 
-      <p>{{$produit->image->count()}}</p>
-      @endif
+      <div class="container">
+
+        @foreach ($produit->image as $image)
+            
+      <div class="mySlides">
+        <div class="numbertext">1 / 6</div>
+        <img src="{{$image->image}}" style="width:100% ;height:250px;display: block; margin-left: auto; margin-right: auto ">
+      </div>
+    
+         @endforeach
+    
+      <a class="prev" onclick="plusSlides(-1)">❮</a>
+      <a class="next" onclick="plusSlides(1)">❯</a>
+    
+     
+    
+      <div class="row" >
+    
+        @foreach ($produit->image as $image)
+    <div style="  margin: auto;  text-align: center;">
+        <div class="column">
+          <img class="demo cursor" src="{{$image->image}}" style="width:50px; height:50px;" onclick="currentSlide({{$loop->index+1}})" alt="The Woods">
+       </div> </div>
+        @endforeach
+    
+      </div>
+    </div>
+    
+    <script>
+    var slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      var dots = document.getElementsByClassName("demo");
+      var captionText = document.getElementById("caption");
+      if (n > slides.length) {slideIndex = 1}
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+      }
+      for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += " active";
+    }
+    </script>
+          @endif
 
 
       <div class="card-body">
