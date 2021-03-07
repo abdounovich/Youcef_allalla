64e251e40122ae558def836032a15eab4f85d7ec
1 <abdounovich@gmail.com>
2021-03-07 19:51:06 +0100

    comment

--- resources/views/images/add.blade.php
+++ resources/views/images/add.blade.php
@@ -21,7 +21,7 @@
    @foreach ($product->image as $image)   
     <div class="col mt-5"><img class="img img-fluid mx-auto d-block" id="image" 
 src="{{$image->image}}"
- alt="" width="100" height="200" >
+ alt=""  >
  
     </div>
    @endforeach
