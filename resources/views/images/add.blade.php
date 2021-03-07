2177f132e03f9de3a95e26986a8ed86bdae2e098
1 <abdounovich@gmail.com>
2021-03-07 19:53:32 +0100

    comment

--- resources/views/images/add.blade.php
+++ resources/views/images/add.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 
 @if (\Session::has('success'))
-    <div class="alert  alert-info p-2 m-4  ">
+    <div class="alert  alert-info p-2 m-4 mt-5  ">
         <ul>
             <li class="p-2  text-left">{!! \Session::get('success') !!}</li>
         </ul>
