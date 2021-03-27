c345c3fec1a7207998dbd5765aa7db357cfe51b0
1 <abdounovich@gmail.com>
2021-03-27 13:53:57 +0100

    comment

--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -9,8 +9,7 @@
 <hr color="white">
     <div class=" bg-dark  " >
         <a class="  btn-circle float-right " >
-            <span class="text-white float-right h3 ">{{$commande->slug}}</span> 
-                    <span class="text-white h4 ">{{$commande->product->code_interne}}</span> 
+            <span class="text-white float-right h4 ">{{$commande->slug}} -- {{$commande->product->code_interne}}</span> 
 </a>
         
        
