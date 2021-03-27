355b75f9eeea537a2f0da2255f2b4ff926db17ae
1 <abdounovich@gmail.com>
2021-03-27 14:06:56 +0100

    comment

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -2,7 +2,11 @@
    $remises=App\Remise::where("product_id",$commande->product->id)->first();
    date_default_timezone_set("Africa/Algiers");
     carbon\Carbon::setLocale('fr');@endphp   
-   <div class="mt-2" ><span class="  text-secondary float-left">{{$commande->created_at->DiffForHumans()}}  <span class="  text-secondary float-right">{{$commande->product->code_interne}}</span></span>
+   <div class="my-2" >
+     <span class="  text-secondary float-left">{{$commande->created_at->DiffForHumans()}}  
+  </span>
+  <span class="  text-secondary float-right">{{$commande->product->code_interne}}  
+  </span>
 </div>
  
    @if (!$remises) 
