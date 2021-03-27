49f79c5535da4d393ac2f06efa50014ed546083e
1 <abdounovich@gmail.com>
2021-03-27 14:12:47 +0100

    comment

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -4,7 +4,7 @@
     carbon\Carbon::setLocale('fr');@endphp   
    <div class="my-2" ><span class="  text-secondary ">{{$commande->created_at->DiffForHumans()}}  
   </span>
-  <span class="  text-secondary ">{{$commande->product->code_interne}}  
+  <span class="  text-secondary float-right">{{$commande->product->code_interne}}  
   </span>
   
 </div>
