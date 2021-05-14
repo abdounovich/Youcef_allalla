e20a339f85e01d403b9a22719eb9ea8dc4372081
1 <abdounovich@gmail.com>
2021-05-15 00:20:56 +0200

    comment

--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -9,7 +9,7 @@
 <hr color="white">
     <div class=" bg-dark  " >
         <a class="  btn-circle float-right " >
-            <span class="text-white float-right h4 "> {{$commande->delivery_type}} - {{$commande->slug}}</span> 
+            <span class="text-white float-right h5 "> {{$commande->delivery_type}} - {{$commande->slug}}</span> 
 </a>
         
        
