19990bc49eeccca21b448d0ae2bc6b1ee53bb40a
1 <abdounovich@gmail.com>
2021-05-14 12:42:57 +0200

    comment

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -138,25 +138,7 @@ return redirect()->route('commandes');
     {
         
         $commande=Commande::find($id);
-        if ($commande->product->product_type=="simple") {
-
-            $produit=Product::find($commande->product->id);
-            $produit->quantity=$produit->quantity+$commande->quantity;
-            $produit->save();
-
-        }
-        elseif ($commande->product->product_type=="taille") {
-
-            $produit=Taille::find($commande->taille);
-            $produit->quantity=$produit->quantity+$commande->quantity;
-            $produit->save();
-        } 
-        elseif ($commande->product->product_type=="color") {
-            $produit=Color::find($commande->color);
-            $produit->quantity=$produit->quantity+$commande->quantity;
-            $produit->save();
-
-        } 
+       
         $commande->delete();
 
 return back()->with("success","commande supprimé avec success"); 
