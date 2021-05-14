08e366f3b1d9f80c6793bdc997cc1f44f684cc1c
1 <abdounovich@gmail.com>
2021-05-15 01:40:06 +0200

    comment

--- routes/web.php
+++ routes/web.php
@@ -92,7 +92,7 @@ Route::get('client/{id}', 'ClientController@show')->name('client.show');
 Route::get('commandes', 'CommandeController@index')->name('commandes');
 Route::post('commandes/add', 'CommandeController@store')->name('commandes.add');
 Route::get('commandes/edit/{id}', 'CommandeController@edit')->name('commandes.edit');
-Route::get('commandes/update/{id}', 'CommandeController@update')->name('commandes.update');
+Route::post('commandes/update/{id}', 'CommandeController@update')->name('commandes.update');
 
 Route::post('commandes/edit/{id}', 'CommandeController@update')->name('commandes.edit');
 Route::get('commandes/delete/{id}', 'CommandeController@destroy')->name('commandes.delete');
