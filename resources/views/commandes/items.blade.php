836785aaccb5ef78d4c150efafedd68f8577bafe
1 <abdounovich@gmail.com>
2021-05-15 11:56:33 +0200

    comment

--- resources/views/commandes/items.blade.php
+++ resources/views/commandes/items.blade.php
@@ -67,7 +67,9 @@
 @endIf
     
    <div class="d-flex justify-content-between">
-   <div class=" badge badge-danger mt-2 text-white h5 p-2 m-2  float-left " >{{$commande->delivery_type}}</div>  
+   <div class=" badge badge-danger mt-2 text-white h5 p-2 m-2  float-left " @if ($commande->delivery_type=="home")
+       class="badge-success"
+   @endif >{{$commande->delivery_type}}</div>  
    
    
             <div class="badge badge-primary mt-2 text-white h5 p-2 m-2  float-left ">{{$commande->yalidine_TN}}</div>
