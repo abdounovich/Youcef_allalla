2445077f043b120148a1235b194a0b2d018bdb63
1 <abdounovich@gmail.com>
2021-05-24 22:49:52 +0200

    comment

--- resources/views/commandes/links.blade.php
+++ resources/views/commandes/links.blade.php
@@ -115,20 +115,20 @@
   @endphp
 <hr class="bg-white ">
         @if ($commande->type=="1" OR $commande->type=="2"  OR $commande->type=="6" )
-        <a class="btn btn-danger  btn-circle float-left   mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
+        <a class="btn btn-danger  btn-circle float-left mb-3  mr-1" href="{{route('commandes.annuler',$commande->id)}}"  >
             <span class="     fa fa-remove  "></span>
         </a>
         @endif
         
         @if ($commande->type=="2")
-        <a class="btn btn-success btn-circle float-left    mr-1" href="{{route('commandes.delivration',$commande->id) }}">
+        <a class="btn btn-success btn-circle float-left mb-3   mr-1" href="{{route('commandes.delivration',$commande->id) }}">
             <span class="  fa fa-truck   "></span>
           
         </a> 
 
       
         
-        <a class="btn btn-warning btn-circle float-left  mr-1" href="{{route('commandes.return',$commande->id) }}">
+        <a class="btn btn-warning btn-circle float-left mb-3  mr-1" href="{{route('commandes.return',$commande->id) }}">
             <span class="    fa fa-refresh    "></span>
         </a>
         @endif
