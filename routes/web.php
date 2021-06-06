1b9aae4567c2a360611eb7b6acc43d9d0a6ae6df
1 <abdounovich@gmail.com>
2021-06-06 21:28:53 +0200

    comment

--- routes/web.php
+++ routes/web.php
@@ -20,41 +20,7 @@ use Illuminate\Database\Eloquent\Builder;
 
 Route::get('/test', function () {
 
-    return '<!DOCTYPE html>
-    <html>
-    <head>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="{{ asset("js/app.js") }}" type="text/js"></script>   
-    
-    <script>
-    $(document).on("pageinit",function(){
-    
-      var pathname = $(location).attr("href");
-      var leva = $("#le").attr("href");
-      var prava = $("#pr").attr("href");
-    
-      $("body").on("swiperight",function(){
-        <!--alert("You swiped right!");-->
-        location.href=prava;
-      });                       
-      $("body").on("swipeleft",function(){
-        <!--alert("You swiped left!");-->
-        location.href=leva;
-      });
-    });
-    </script>
-    </head>
-    <body>
-    
-    
-    <div id="test_div" style="display: block;">
-        <img src="test.gif" width="100px">
-        <a id="le" href="http://sleepy-garden-77812.herokuapp.com/products/">left</a>
-        <a id="pr" href="http://sleepy-garden-77812.herokuapp.com/clients">right</a>
-    </div>';
-
+    return view("test");
 });
 
 
