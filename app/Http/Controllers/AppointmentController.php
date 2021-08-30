d29303fb779e90e2fded0ad351ac4f9d2769ca0d
1 <abdounovich@gmail.com>
2021-08-30 20:20:39 +0200

    comment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -50,6 +50,7 @@ $appointment=Appointment::find($id)->delete();
     public function parametres(Request $request)
     {
 
+        dd('rrrr');
         $anglais = ['Saturday' ,'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];  
  
     for ($i = 0; $i < 7; $i++){
