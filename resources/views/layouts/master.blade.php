b66eeaf05f8bca05ef848f7f04b113998c972f8a
1 <abdounovich@gmail.com>
2021-08-31 22:55:02 +0200

    comment

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,8 +26,45 @@
 
       
 
- 
+.overlay{
+    display: none;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index: 999;
+    background: rgba(255,255,255,0.8) url("loader.gif") center no-repeat;
+}
+/* Turn off scrollbar when body element has the loading class */
+body.loading{
+    overflow: hidden;   
+}
+/* Make spinner image visible when body element has the loading class */
+body.loading .overlay{
+    display: block;
+}
     </style>
+
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script>
+// Initiate an Ajax request on button click
+$(document).on("click", "button", function(){
+    $.get("customers.php", function(data){
+        $("body").html(data);
+    });       
+});
+ 
+// Add remove loading class on body element based on Ajax request status
+$(document).on({
+    ajaxStart: function(){
+        $("body").addClass("loading"); 
+    },
+    ajaxStop: function(){ 
+        $("body").removeClass("loading"); 
+    }    
+});
+</script>
     </head>
     <body dir="rtl" style="    
     background:url('{{Setting::get('theme.bg-image') }}');
@@ -36,7 +73,8 @@
     background-size: cover;
     font-family: 'Cairo', sans-serif">
       
-     
+      <div class="overlay"></div>
+
         <div>   @if ($errors->any())
             <div class="alert alert-danger">
               <ul>
