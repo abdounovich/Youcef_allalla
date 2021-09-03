e30289a3bddd8ddd79c3d888e86c8c649f93e498
1 <abdounovich@gmail.com>
2021-09-03 22:31:17 +0200

    comment

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,6 @@
        
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta http-equiv="refresh" content="250">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Document</title>      
 
