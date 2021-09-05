f2d7434dd2c664756699c77e46b6f6bcb290f93d
1 <abdounovich@gmail.com>
2021-09-05 22:17:58 +0200

    comment

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -160,7 +160,7 @@ date_default_timezone_set("Africa/Algiers");
           <div class="timeline-content-item-reveal">
               <a href="#">
                   <img src="https://picsum.photos/g/300/300">
-                  <span>Lorem Ipsum</span>
+                  <span>{{$appointment->facebook}}</span>
               </a>
           </div>
       </div>              @endforeach
