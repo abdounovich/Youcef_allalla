e12b74d4740c45be916b2809ad480acc4723e880
1 <abdounovich@gmail.com>
2021-09-06 22:27:45 +0200

    comment

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -31,11 +31,11 @@
     <table class="table table-striped table-dark responsive" style="opacity:0.9 ">
       <thead class=" bg-success text-right">
         <tr>
+          <th scope="col"></th>
 
           <th scope="col">الفيسبوك</th>
           <th scope="col"> الحلاقة </th>
           <th scope="col">الموعد  </th>
-          <th scope="col"></th>
 
 
 
