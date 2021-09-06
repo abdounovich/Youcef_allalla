9df21349570d79714f194a277b3d207e3fffde5b
1 <abdounovich@gmail.com>
2021-09-06 22:42:53 +0200

    comment

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -108,13 +108,14 @@
                      
           
             </td>         
-          <td  class="align-middle clearfix" style="position: relative;"><img class=" border rounded-circle ml-5" width="50" height="50" src="{{$picture}}" alt="">
+          <td  class="align-middle clearfix" style="position: relative;">
             <span  dir="ltr" style=" 
             display:inline-flex;
             width: 100px;
             overflow: hidden;
             white-space: nowrap;
-          text-overflow: ellipsis;"><a  class="text-white" href="sendMsg/{{$Today_appointment->fb_id}}">{{$Today_appointment->facebook}}</a></span>  <span dir="ltr" style=" position: absolute;
+          text-overflow: ellipsis;"><a  class="text-white" href="sendMsg/{{$Today_appointment->fb_id}}">{{$Today_appointment->facebook}}</a></span> <img class=" border rounded-circle ml-5" width="50" height="50" src="{{$picture}}" alt="">
+         <span dir="ltr" style=" position: absolute;
             top:1px;
             font-size:10px;
             right:1px; width:30px;height:30px; 
